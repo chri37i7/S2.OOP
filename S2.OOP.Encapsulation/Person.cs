e96a1ca7941bf816bc1e0a5af6aed61a33ca9c3f
Christian Alexander Heilmann Bennedbæk <chri37i7@edu.campusvejle.dk>
2020-02-24 13:49:47 +0100

    Add project files.

---
+++ S2.OOP.Encapsulation/Person.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace S2.OOP.Encapsulation
+{
+    class Person
+    {
+        // Fields
+        private string firstname;
+        private string lastname;
+        private DateTime birthday;
+        private string cpr;
+
+
+        // Constructor
+        public Person(string firstname, string lastname, DateTime birthday, string cpr)
+        {
+            Firstname = firstname;
+
+            Birthday = birthday;
+            Cpr = cpr;
+        }
+
+        // Properties
+        public string Firstname
+        {
+            get
+            {
+                return firstname;
+            }
+            set
+            {
+                if(value.All(c => Char.IsWhiteSpace(c)))
+                {
+                    throw new ArgumentException("The name must not be all spaces", nameof(Firstname));
+                }
+                if(string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentNullException(nameof(Firstname), "The name cannot be empty");
+                }
+                if(value.Length < 1)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(Firstname), "The name cannot be lower than one character");
+                }
+                if(value != Firstname)
+                {
+                    value = Firstname;
+                }
+            }
+        }
+
+        public DateTime Birthday
+        {
+            get
+            {
+                return birthday;
+            }
+            set
+            {
+                birthday = value;
+            }
+        }
+
+        public string Cpr
+        {
+            get
+            {
+                return cpr;
+            }
+            set
+            {
+                cpr = value;
+            }
+        }
+
+        public enum Gender
+        {
+            Male,
+            Female,
+            Unspecified
+        }
+    }
+}
