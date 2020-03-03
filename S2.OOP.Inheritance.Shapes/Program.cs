49f6afa99c30ea30821af0fc295b7a49197864b5
Christian Alexander Heilmann Bennedbæk <chri37i7@edu.campusvejle.dk>
2020-03-03 11:07:39 +0100

    Added Shapes, and some organisation to the projects

---
+++ S2.OOP.Inheritance.Shapes/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace S2.OOP.Inheritance.Shapes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
